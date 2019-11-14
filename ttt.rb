5456b62ab8c464cde1314484dc7943fd9e81149b
John Isom <John.Isom2001@gmail.com>
2019-11-14 13:09:36 -0700

    Add basic pages and layouts

--- ttt.rb
+++ ttt.rb
@@ -2,6 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_key, 'secret'
+end
+
 get '/' do
-  '<a href="https://github.com/johnisom/sinatra_ttt">Github repo</a>'
+  erb :index
+end
+
+get '/play' do
+  erb :play
+end
+
+post '/play' do
+  session[:flash] = "Button #{params[:box]} clicked!"
+  erb :play
 end
